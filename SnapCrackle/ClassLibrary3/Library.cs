5ea6076b2c02c95c5354647207b3ad9e1b64f417
turboPocky <turboPocky@gmail.com>
2015-11-18 07:33:19 -0600

    SnapCrackle code exercise
    SnapCrackle as class library with unit tests

---
+++ SnapCrackle/ClassLibrary3/Library.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Test
+{
+    public class Class1
+    {
+        [ExcludeFromCodeCoverage]
+        public static void Main(params string[] args)
+        {
+            // print 1-100 
+            // divisible by 3 = snap 
+            // divisible by 5 = crackle 
+            // divisible by 3 and 5 = snap crackle 
+
+            var snapCrackle = new Library.SnapCrackle(DelegateMethod);
+
+            var wordPairs = new List<Library.DenominatorWordPair>();
+            wordPairs.Add(new Library.DenominatorWordPair { Denominator = 2, Word = "bob" });
+            wordPairs.Add(new Library.DenominatorWordPair { Denominator = 3, Word = "sally" }); 
+
+            snapCrackle.PrintResults(1, 100, wordPairs); 
+
+            Console.ReadKey();
+        }
+
+        public static void DelegateMethod(string message)
+        {
+            System.Console.Write(message);
+        }
+    }
+}
+
+namespace Library
+{
+    public class DenominatorWordPair
+    {
+        public int Denominator { get; set; }
+        public string Word { get; set; }
+    }
+
+    public class SnapCrackle
+    {
+        public delegate void OutputDelegate(string message);
+
+        private OutputDelegate _logger { get; set; }
+
+        public SnapCrackle(OutputDelegate logger)
+        {
+            _logger = logger; 
+        }
+
+        // pass in list of denominators and words 
+        // 2 bob
+        // 3 sam
+        // 6 sally 
+
+        // unit test this 
+        // send to zack a link to github 
+        
+        public void PrintResults(int start, int end, IEnumerable<DenominatorWordPair> wordPairs) 
+        {
+            if (!(start < end))
+            {
+                throw new InvalidOperationException("start must be less than end"); 
+            }
+
+            if (wordPairs == null || wordPairs.Count() == 0)
+            {
+                throw new ArgumentException("wordPairs must have at least one item");
+            }
+
+            for (var i = start; i <= end; i++)
+            {
+                _logger(string.Format("{0} ", i));
+
+                foreach (var wordPair in wordPairs)
+                {
+                    if (i % wordPair.Denominator == 0)
+                    {
+                        _logger(wordPair.Word); 
+                    }
+                }
+
+                _logger(System.Environment.NewLine);
+            }
+        }
+    }
+}
+
---
+++ SnapCrackle/SnapCrackle.Test/SnapCrackleTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using NUnit.Common;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SnapCrackle.Test
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class SnapCrackleTest
+    {
+        public static void OutputDelegate(string message)
+        {
+           // Debug.Write(message);
+        }
+
+        [TestMethod]
+        public void Test_PrintResultsThrowsIfStartNotLessThanEnd()
+        {
+            var snapCrackle = new Library.SnapCrackle(SnapCrackleTest.OutputDelegate);
+
+            var start = 10;
+            var end = 8;
+            var denominatorList = new List<Library.DenominatorWordPair>(new[] { new Library.DenominatorWordPair { Word = "Test", Denominator = 2 }});
+
+            NUnit.Framework.Assert.Throws<InvalidOperationException>(() => snapCrackle.PrintResults(start, end, denominatorList));
+        }
+
+        [TestMethod]
+        public void Test_PrintResultsThrowsIfDenominatorListIsNull()
+        {
+            var snapCrackle = new Library.SnapCrackle(SnapCrackleTest.OutputDelegate);
+
+            var start = 1;
+            var end = 100;
+            
+            NUnit.Framework.Assert.Throws<ArgumentException>(() => snapCrackle.PrintResults(start, end, null));
+        }
+
+        [TestMethod]
+        public void Test_PrintResultsThrowsIfDenominatorListIsEmpty()
+        {
+            var snapCrackle = new Library.SnapCrackle(SnapCrackleTest.OutputDelegate);
+
+            var start = 1;
+            var end = 100;
+            var denominatorList = new List<Library.DenominatorWordPair>();
+
+            NUnit.Framework.Assert.Throws<ArgumentException>(() => snapCrackle.PrintResults(start, end, denominatorList));
+        }
+                
+        [TestMethod]
+        public void Test_PrintResultsOutputsDenominatorWords()
+        {
+            var outputLog = new StringBuilder();
+            var snapCrackle = new Library.SnapCrackle((s) => { outputLog.Append(s); });
+
+            var start = 1;
+            var end = 10;
+            var denominatorList = new List<Library.DenominatorWordPair>();
+            denominatorList.Add(new Library.DenominatorWordPair { Denominator = 2, Word = "bob" });
+            denominatorList.Add(new Library.DenominatorWordPair { Denominator = 3, Word = "sally" });
+
+            snapCrackle.PrintResults(start, end, denominatorList);
+
+            var output = outputLog.ToString().Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            /* expected: 
+                    1 
+                    2 bob
+                    3 sally
+                    ... 
+                    6 bobsally*/
+
+            NUnit.Framework.Assert.AreEqual(output[0], "1 ");
+            NUnit.Framework.Assert.AreEqual(output[1], "2 bob");
+            NUnit.Framework.Assert.AreEqual(output[2], "3 sally");
+            NUnit.Framework.Assert.AreEqual(output[5], "6 bobsally");
+        }
+
+        [TestMethod]
+        public void Test_PrintResultsOutputsCorrectNumberOfLines()
+        {
+            var outputLog = new StringBuilder();
+            var snapCrackle = new Library.SnapCrackle((s) => { outputLog.Append(s); });
+
+            var start = 1;
+            var end = 10;
+            var denominatorList = new List<Library.DenominatorWordPair>();
+            denominatorList.Add(new Library.DenominatorWordPair { Denominator = 2, Word = "bob" });
+            denominatorList.Add(new Library.DenominatorWordPair { Denominator = 3, Word = "sally" });
+
+            snapCrackle.PrintResults(start, end, denominatorList);
+
+            var output = outputLog.ToString().Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            NUnit.Framework.Assert.AreEqual(output.Length, 10);
+        }
+    }
+}
